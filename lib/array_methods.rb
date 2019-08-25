15030a7c753ca2149a0e0197cb1878c58f35749d
Alfonso Socarras <asocarras10@gmail.com>
2019-08-25 21:21:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,11 +3,10 @@ def find_element_index(array, value_to_find)
   counter = 0
   
   while counter < array.length do
-    p value_to_find[counter]
+    p array(value_to_find)
     counter += 1
   end
   
-  return value_to_find[counter]
 end
 
 
